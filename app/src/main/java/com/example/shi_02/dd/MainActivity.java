83afada520e9599bf00a6e8c6f670f2800c3fb5f
Struggle111 <1549183281@qq.com>
2016-05-24 16:24:40 +0800

    button   点击事件

--- app/src/main/java/com/example/shi_02/dd/MainActivity.java
+++ app/src/main/java/com/example/shi_02/dd/MainActivity.java
@@ -4,14 +4,27 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"dddd",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
